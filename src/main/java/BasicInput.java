d05aa2b4c3a26ac883b6bd3dd501221f8da12353
Bacon <parkerjoshua0403@gmail.com>
2026-01-30 15:46:04 -0500

    Todo 2 and 3 completed

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -18,12 +18,12 @@ public class BasicInput {
         System.out.println("Enter String:");
         userString = scnr.next();
 
-        System.out.printf("%d %f %c %s%n", userInt, userDouble, userChar, userString);
+        System.out.printf("%d %.2f %c %s%n", userInt, userDouble, userChar, userString);
 
-        System.out.printf("%s %c %f %d%n", userString, userChar, userDouble, userInt);
+        System.out.printf("%s %c %.2f %d%n", userString, userChar, userDouble, userInt);
 
         int newInt = (int)userDouble;
-        System.out.print(userDouble + " cast to an integer is " + newInt);
+        System.out.printf("%.2f %s %d", userDouble, "cast to an integer is", newInt);
 
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
 
