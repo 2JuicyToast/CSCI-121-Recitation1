91451e91732773f6138e208247eba1652cc6f4a6
Bacon <parkerjoshua0403@gmail.com>
2026-01-30 15:33:46 -0500

    Todo 2 and 3 completed

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -20,6 +20,11 @@ public class BasicInput {
 
         System.out.printf("%d %f %c %s%n", userInt, userDouble, userChar, userString);
 
+        System.out.printf("%s %c %f %d%n", userString, userChar, userDouble, userInt);
+
+        int newInt = (int)userDouble;
+        System.out.print(userDouble + " cast to an integer is " + newInt);
+
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
 
         // TODO (2): Output the four values in reverse
